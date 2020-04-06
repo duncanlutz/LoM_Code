cd2ded539090e1a8cd737ba14afe343b94e177e3
cameronporter89 <63262321+cameronporter89@users.noreply.github.com>
2020-04-06 15:26:41 -0600

    Add files via upload

---
+++ cameracontroller.cs
@@ -0,0 +1,30 @@
+﻿
+using UnityEngine;
+
+public class cameracontroller : MonoBehaviour
+{
+	public float panSpeed = 20f;
+	public float panBorderThickness = 10f;
+
+    // Update is called once per frame
+    void Update()
+    {
+		Vector3 pos = transform.position;
+		
+        if (Input.GetKey("w") || Input.mousePosition.y >= Screen.height - panBorderThickness)
+		{
+			pos.z += panSpeed * Time.deltaTime;
+		}
+		if (Input.GetKey("s") || Input.mousePosition.y <= panBorderThickness)
+		{
+			pos.z -= panSpeed * Time.deltaTime;
+		}if (Input.GetKey("d") || Input.mousePosition.x >= Screen.width - panBorderThickness)
+		{
+			pos.x += panSpeed * Time.deltaTime;
+		}if (Input.GetKey("a") || Input.mousePosition.x <= panBorderThickness)
+		{
+			pos.x -= panSpeed * Time.deltaTime;
+		}
+		transform.position = pos;
+    }
+}
---
+++ camerafollowrotate.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class camerafollowrotate : MonoBehaviour
+{
+	public Rigidbody rb;
+	public Transform target;
+	
+	public float forwardForce = 500f;
+	public float sidewaysForce = 500f;
+
+    // use fixedupdate because Unity prefers that
+	
+    void FixedUpdate()
+    {
+		if(Input.GetKey("w"))
+		{
+        rb.AddForce(0, forwardForce * Time.deltaTime, 0, ForceMode.VelocityChange);
+		}
+		if(Input.GetKey("s"))
+		{
+			rb.AddForce(0, -forwardForce * Time.deltaTime, 0, ForceMode.VelocityChange);
+		}
+		if (Input.GetKey("d"))
+		{ 
+			rb.AddForce(sidewaysForce * Time.deltaTime, 0, 0, ForceMode.VelocityChange);
+		}	
+		if (Input.GetKey("a"))
+		{ 
+			rb.AddForce(-sidewaysForce * Time.deltaTime, 0, 0, ForceMode.VelocityChange);
+		}	
+	transform.LookAt(target);
+    }
+}
